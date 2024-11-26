393b3cd29928191ae9d6ac2c263ab0d3af89afb4
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-11-26 17:19:32 +0100

    footer - started

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,13 +78,59 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Liens utiles',
+          items: [  
+            {
+              html: `
+                <a href="https://www.helloasso.com/associations/amicale-des-donneurs-de-sang-benevoles-de-lyon-adsbl" target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Adhérer à notre amicale
+                </a>
+              `,
+            },
+            {
+              html: `
+                <a target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Site de l'Union Départementale (à venir)
+                </a>
+              `,
+            },
+            {
+              html: `
+                <a href="https://ffdsb.org/" target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Site de la Fédération
+                </a>
+              `,
+            },
+          ],
+        },
+        {
+          title: 'Outils',
           items: [
             {
               html: `
-                <a href="https://github.com/youraccount" target="_blank" rel="noopener noreferrer" style="display:flex;align-items:center;">
-                  <img src="/img/logo.png" alt="GithhHub" style="width:24px;height:24px;" />
-                  <span style="color:white; margin-left:8px;">Accueil</span>
+                <a href="https://doodle.com/sign-up-sheet/participate/aab185ba-e804-4519-89fd-f1997581b7dc/select" target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Prochaines collectes
+                </a>
+              `,
+            },
+            {
+              html: `
+                <a target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Comptabilité (à venir)
+                </a>
+              `,
+            },
+            {
+              html: `
+                <a target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Statistiques (à venir)
+                </a>
+              `,
+            },
+            {
+              html: `
+                <a target="_blank" rel="noopener noreferrer" style="color:white;">
+                  Ajout de collecte (à venir)
                 </a>
               `,
             },
@@ -95,12 +141,16 @@ const config: Config = {
           items: [
             {
               html: `
-              <a href="https://twitter.com/youraccount" target="_blank" rel="noopener noreferrer">
-                <img src="/img/logo.png" alt="Twitter" style="width:24px;height:24px;margin-right:8px;" />
-              </a>
-              <a href="https://github.com/youraccount" target="_blank" rel="noopener noreferrer">
-                <img src="/img/logo.png" alt="GitHub" style="width:24px;height:24px;" />
-              </a>
+              <div class="flex flex-col">
+                <a href="mailto:adsbl69@hotmail.com" target="_blank" rel="noopener noreferrer" class="flex items-center w-fit" >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"><path fill="white" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z"/></svg>
+                  <span style="color:white; margin-left:8px;">Email</span>
+                </a>
+                <a href="https://www.instagram.com/dondusanglyon/" target="_blank" rel="noopener noreferrer" class="flex items-center w-fit">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 14 14"><path fill="white" fill-rule="evenodd" d="M3.39.787A2.604 2.604 0 0 0 .786 3.39v6.944a2.604 2.604 0 0 0 2.604 2.604h6.944a2.604 2.604 0 0 0 2.603-2.604V3.39A2.604 2.604 0 0 0 10.334.787zm7.693 2.607a.75.75 0 1 1-1.5 0a.75.75 0 0 1 1.5 0m-4.22 1.388a2.08 2.08 0 1 0 0 4.16a2.08 2.08 0 0 0 0-4.16m-3.081 2.08a3.08 3.08 0 1 1 6.16 0a3.08 3.08 0 0 1-6.16 0" clip-rule="evenodd"/></svg>
+                  <span style="color:white; margin-left:8px;">Instagram</span>
+                </a>
+              </div>
               `,
             },
           ],
