0c5df0fda96f66a1a80fa23868f02f2724083f1d
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-06-03 12:07:02 +0200

    vercel analytics

---
+++ src/theme/Layout/index.jsx
@@ -0,0 +1,13 @@
+// src/theme/Layout/index.tsx
+import React from 'react';
+import Layout from '@theme-original/Layout';
+import { Analytics } from '@vercel/analytics/react';
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Layout {...props} />
+      <Analytics />
+    </>
+  );
+}
