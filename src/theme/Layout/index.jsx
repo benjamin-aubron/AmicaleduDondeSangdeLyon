6008c0fec1a123db3da4f928508b885483dbd9f0
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-07-29 15:37:21 +0200

    speedinsights added

--- src/theme/Layout/index.jsx
+++ src/theme/Layout/index.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <Layout {...props} />
       <Analytics />
+      <SpeedInsights/>
     </>
   );
 }
