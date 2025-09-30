4dfe6974c31e1b518c1a7ad9da4c49b99a321bf3
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-30 16:49:38 +0200

    update - collecte nov 2025

---
+++ blog/2025-09-26 Collecte Lyon 4.md
@@ -0,0 +1,9 @@
+---
+slug: collect-lyon4-sep-2025
+title: Collecte à Lyon 4 - Salle de la Ficelle
+tags: [Collecte, Lyon]
+---
+
+#### Résultats de la collecte
+
+Amine a assuré, à lui seul, l'accueil de cette collecte ainsi que le remplissage des pichets d'eau pour les donneurs. Un grand merci pour son engagement. Au cours de cette collecte, sur **150 attendus**, il y a eu **125 présentés**, **104 prélevés** dont **6 primodonneurs**.
---
+++ src/db/listCollectes.ts
@@ -0,0 +1,180 @@
+export const collectsList = [
+  {
+    mois: "Janvier",
+    date: "12/01/2024",
+    type_collecte: "Collecte Mobile",
+    lieu: "Bron - Espace Roger Pestourie",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "110,102,91,6"
+  },
+  {
+    mois: "Février",
+    date: "06/02/2024",
+    type_collecte: "Collecte Evènementielle",
+    lieu: "Lyon 5 - ECAM",
+    heure: "9h-14h",
+    statut: "Confirmée",
+    results: "nan,nan,113,53"
+  },
+  {
+    mois: "Février",
+    date: "13/02/2024",
+    type_collecte: "Collecte Evènementielle",
+    lieu: "Lyon 2 - Palais de la Bourse",
+    heure: "9h-14h et 15h-19h",
+    statut: "Confirmée",
+    results: "840,831,681,145"
+  },
+  {
+    mois: "Mars",
+    date: "22/03/2025",
+    type_collecte: "Collecte Evènementielle",
+    lieu: "Lyon 3 - Centre Commercial",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "nan,164,125,15"
+  },
+  {
+    mois: "Janvier",
+    date: "03/01/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Bron - Espace Roger Pestourie",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "110,105,87,10"
+  },
+  {
+    mois: "Janvier",
+    date: "10/01/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Villeurbanne - CCVA",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "140,119,98,3"
+  },
+  {
+    mois: "Janvier",
+    date: "17/01/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 4 - Salle de la Ficelle",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "150,119,103,8"
+  },
+  {
+    mois: "Janvier",
+    date: "27/01/2025",
+    type_collecte: "Collecte Événementielle",
+    lieu: "Auditorium de Lyon",
+    heure: "9h-14h et 15h-19h",
+    statut: "Confirmée",
+    results: "255,235,206,30"
+  },
+  {
+    mois: "Février",
+    date: "05/02/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 8 - Mairie du 8ème",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "105,96,86,6"
+  },
+  {
+    mois: "Février",
+    date: "19/02/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Villeurbanne - CCVA",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "150,127,113,16"
+  },
+  {
+    mois: "Mars",
+    date: "11/03/2025",
+    type_collecte: "Collecte Événementielle",
+    lieu: "Palais de la Bourse",
+    heure: "9h-14h et 15h-19h",
+    statut: "Confirmée",
+    results: "nan,416,332,63"
+  },
+  {
+    mois: "Mars",
+    date: "12/03/2025",
+    type_collecte: "Collecte Événementielle",
+    lieu: "Palais de la Bourse",
+    heure: "9h-14h et 15h-19h",
+    statut: "Confirmée",
+    results: "nan,416,332,63"
+  },
+  {
+    mois: "Mars",
+    date: "21/03/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Bron - Espace Roger Pestourie",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "110,104,91,3"
+  },
+  {
+    mois: "Avril",
+    date: "11/04/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 6 - Mairie du 6ème",
+    heure: "9h-13h et 15h-19h",
+    statut: "Confirmée",
+    results: "150,139,116,16"
+  },
+  {
+    mois: "Avril",
+    date: "25/04/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 4 - Salle de la Ficelle",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "120,93,79,7"
+  },
+  {
+    mois: "Juin",
+    date: "18/06/2025",
+    type_collecte: "Collecte Événementielle",
+    lieu: "Hôtel de ville",
+    heure: "9h-14h et 15h30-19h",
+    statut: "Confirmée"
+  },
+  {
+    mois: "Juin",
+    date: "19/06/2025",
+    type_collecte: "Collecte Événementielle",
+    lieu: "Hôtel de ville",
+    heure: "9h-14h et 15h30-19h",
+    statut: "Confirmée"
+  },
+  {
+    mois: "Juin",
+    date: "24/06/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Villeurbanne - CCVA",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "150,96,74,18"
+  },
+  {
+    mois: "Septembre",
+    date: "26/09/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 4 - Salle de la Ficelle",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: "150,125,104,6"
+  },
+  {
+    mois: "Novembre",
+    date: "21/11/2025",
+    type_collecte: "Collecte Mobile",
+    lieu: "Lyon 4 - Salle de la Ficelle",
+    heure: "15h-19h",
+    statut: "Confirmée",
+    results: ""
+  }
+]
